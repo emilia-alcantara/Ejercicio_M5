a960ca7d9c99e20fe1a1965e814dd0e387fff034
Emilia <correoalcantaraemilia@gmail.com>
2023-07-01 19:35:39 -0400

    picasso

--- app/src/main/java/cl/individual/ejerciciom5/MainActivity.java
+++ app/src/main/java/cl/individual/ejerciciom5/MainActivity.java
@@ -1,6 +1,7 @@
 package cl.individual.ejerciciom5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.CircularArray;
 
 import android.os.Bundle;
 
@@ -14,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Picasso
+                .get()
+                .load("https://images.unsplash.com/photo-1476994230281-1448088947db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZsb3dlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60")
+                .into(binding.imageView);
     }
 }
